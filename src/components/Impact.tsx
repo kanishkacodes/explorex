aa008bfff3c54e0376e887ddec8c2d3d419470b6
Kanishka Kumar <kumarkanishka2111@gmail.com>
2024-05-28 16:31:48 +0530

    primary colour changes

--- src/components/Impact.tsx
+++ src/components/Impact.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Impact() {
   return (
-    <div className="h-[40rem] flex items-center justify-center   bg-gradient-to-tr from-[#582460] to-[#1b0127]">
+    <div className="h-[40rem] flex items-center justify-center   bg-gradient-to-tr from-[#44065a] to-[#120119]">
       <div className="text-center">
         <h1 className=" md:text-5xl text-3xl mb-10 mr-5 ml-5 font-bold text-white  tracking-wide">Let’s create a measurable impact on your business.</h1>
         <button className="py-4 px-14 bg-white font-bold text-black md:text-2xl text-xl   rounded-lg  tracking-wide hover:bg-black hover:text-white">Design A Quote</button>
