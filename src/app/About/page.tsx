9852b7b9f7f45cc9c7022c0171f9bd1e6383c05f
Kanishka Kumar <kumarkanishka2111@gmail.com>
2024-06-05 01:46:59 +0530

    font fixed

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -31,16 +31,9 @@ const Hero: React.FC = () => {
           animate={controls}
           variants={textVariants}
           transition={{ duration: 0.5, delay: 0.25 }}
-          className="md:w-1/3 mt-8 md:mt-0 order-2 md:order-1" // Adjusted width and order
+          className="mr-0 md:mr-9 w-full md:w-auto"
         >
-          <h1 className="font-bold tracking-wide text-3xl md:text-6xl text-black md:mb-8 mb-4">About Us</h1>
-          <p className="text-lg md:text-2xl tracking-wide text-neutral-600 font-medium">
-            Thinkfluence is a premier bespoke consultancy firm focusing only in the Hospitality domain. We are an extension
-            of Explorex, which is India&apos;s leading full stack OS which helps in managing your restaurant more efficiently
-            thereby helping you generate more revenue. We specialise in Performance Marketing thereby generating more ROl
-            on your spend, and helping segment & target more effectively. Parallel we believe in crafting unforgettable
-            experiences and optimising operations for businesses in the world of hospitality.
-          </p>
+          <LottieAnimation animationPath="/Assets/Clients.json" />
         </motion.div>
         <motion.div
           ref={ref}
@@ -48,9 +41,16 @@ const Hero: React.FC = () => {
           animate={controls}
           variants={textVariants}
           transition={{ duration: 0.5, delay: 0.5 }}
-          className="w-full md:w-auto order-1 md:order-2" // Adjusted order
+          className="w-full md:w-1/3 mt-8 md:mt-0 " // Adjusted margin top for mobile view
         >
-          <LottieAnimation animationPath="/Assets/Clients.json" />
+          <h1 className="font-bold tracking-wide text-3xl md:text-6xl text-black md:mb-8 mb-4">About Us</h1>
+          <p className="text-lg md:text-xl tracking-wide text-neutral-600 ">
+            Thinkfluence is a premier bespoke consultancy firm focusing only in the Hospitality domain. We are an extension
+            of Explorex, which is India&apos;s leading full stack OS which helps in managing your restaurant more efficiently
+            thereby helping you generate more revenue. We specialise in Performance Marketing thereby generating more ROl
+            on your spend, and helping segment & target more effectively. Parallel we believe in crafting unforgettable
+            experiences and optimising operations for businesses in the world of hospitality.
+          </p>
         </motion.div>
       </div>
       <Footer />
