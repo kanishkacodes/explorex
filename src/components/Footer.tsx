b2335e6667d7f455ea4642a11971e984eead54ec
Kanishka Kumar <kumarkanishka2111@gmail.com>
2024-06-05 11:59:46 +0530

    footer changes minor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className="bg-white text-black  border-t-2 py-12">
-      <div className="container mx-auto flex flex-col lg:flex-row justify-between items-start lg:items-center space-y-8 lg:space-y-0">
+    <footer className=" py-12">
+      <div className="container mx-auto flex flex-col lg:flex-row justify-between items-start lg:items-center space-y-12 lg:space-y-0">
         {/* Logo and Address Section */}
-        <div className="flex flex-col md:w-1/3 space-y-4">
+        <div className="flex flex-col md:w-1/3 space-y-6">
           <Image
             src="/TF.png"
             alt="Logo"
@@ -14,24 +14,23 @@ function Footer() {
             height={184}
             className="w-48 md:w-auto"
           />
-          <p className="text-sm md:text-base">
-            Service Road, 2nd Floor, DVK Spaces, 13th Cross, Dr Puneeth
-            Rajkumar Rd, 6th Sector, HSR Layout, Bengaluru, Karnataka 560102
+          <p className="text-sm md:text-base leading-relaxed">
+            Service Road, 2nd Floor, DVK Spaces, 13th Cross, Dr Puneeth Rajkumar Rd, 6th Sector, HSR Layout, Bengaluru, Karnataka 560102
           </p>
           {/* Social Media Icons */}
           <div className="flex space-x-6">
             <Link href="https://www.facebook.com/profile.php?id=61557601275175">
               <Image
-                src="/facebook.png"
+                src="/f.png"
                 alt="Facebook"
-                width={24}
-                height={24}
+                width={28}
+                height={28}
                 className="hover:opacity-75"
               />
             </Link>
             <Link href="https://www.instagram.com/thinkfluence.byexplorex/">
               <Image
-                src="/instagram.png"
+                src="/i.png"
                 alt="Instagram"
                 width={28}
                 height={28}
@@ -40,6 +39,8 @@ function Footer() {
             </Link>
           </div>
         </div>
+
+        
 
         {/* Business Enquiries */}
         <div className="flex flex-col md:w-1/3 items-start lg:items-end space-y-2">
@@ -51,7 +52,7 @@ function Footer() {
       </div>
 
       {/* Copyright */}
-      <div className="text-center text-xs mt-8">
+      <div className="text-center text-xs mt-12">
         © 2024 Thinkfluence || All Rights Reserved
       </div>
     </footer>
