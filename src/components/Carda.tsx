20f9fd0cf0969b9164b0c7505b01a24a50bbdd64
Kanishka Kumar <kumarkanishka2111@gmail.com>
2024-06-09 15:33:00 +0530

    mt-5

--- src/components/Carda.tsx
+++ src/components/Carda.tsx
@@ -9,7 +9,7 @@ function Carda() {
 
         <div className=' h-screen bg-red-500 hover:bg-red-800'>
           <div className=' h-2/5 flex flex-col px-4 justify-center items-center'>
-          <h1 className="font-bold md:text-5xl text-3xl flex justify-center items-center md:my-10 my-5 text-white">BMW</h1>
+          <h1 className="font-bold md:text-5xl text-3xl flex justify-center items-center md:my-10 my-5 md:mt-5 text-white">BMW</h1>
           <p className="md:text-2xl text-xl tracking-wide font-medium flex justify-center items-center md:my-10 my-5 text-white text-center ">Optimize the effortless on-the-go lifestyle</p>
           
           <div className="md:text-2xl text-xl tracking-wide font-medium flex justify-center items-center space-x-2 md:my-10 my-5 text-white">
