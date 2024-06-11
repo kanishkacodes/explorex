5b84f78c2a14813170880afe54bf801cf9f0029c
Kanishka Kumar <kumarkanishka2111@gmail.com>
2024-06-11 16:51:28 +0530

    updated card

--- src/app/Services/page.tsx
+++ src/app/Services/page.tsx
@@ -1,12 +1,14 @@
 import Services from "@/components/Services";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Cardmain from "@/components/Cardmain";
 
 function page() {
   return (
     <div>
         <Navbar />
         <Services />
+        <Cardmain />
         <Footer />
       
     </div>
