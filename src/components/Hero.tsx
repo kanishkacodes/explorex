4ee2f4814056755cb02c3baf900218e7312ce223
Kanishka Kumar <kumarkanishka2111@gmail.com>
2024-06-12 11:56:04 +0530

    fixed navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,14 +21,14 @@ const Hero: React.FC = () => {
 
   return (
     <div>
-      <div className="lg:flex justify-center space-x-6 ">
+      <div className="md:flex justify-center space-x-6">
         <motion.div
           ref={ref}
           initial="hidden"
           animate={controls}
           variants={textVariants}
           transition={{ duration: 0.5, delay: 0.25 }}
-          className="mr-0 lg:mr-9"
+          className="mr-0 md:mr-6 lg:mr-9"
         >
           <LottieAnimation animationPath="/Assets/OnlineChat.json" />
         </motion.div>
@@ -39,19 +39,19 @@ const Hero: React.FC = () => {
           variants={textVariants}
           transition={{ duration: 0.5, delay: 0.5 }}
         >
-          <h1 className="font-bold tracking-wide text-5xl lg:text-7xl text-black mt-0 lg:mt-28">
+          <h1 className="font-bold tracking-wide text-5xl md:text-6xl lg:text-7xl text-black mt-0 md:mt-14 lg:mt-28">
             Design
           </h1>
-          <h1 className="font-bold tracking-wide text-5xl lg:text-7xl text-black">
+          <h1 className="font-bold tracking-wide text-5xl md:text-6xl lg:text-7xl text-black">
             Transform
           </h1>
-          <h1 className="font-bold tracking-wide text-5xl lg:text-7xl text-black">
+          <h1 className="font-bold tracking-wide text-5xl md:text-6xl lg:text-7xl text-black">
             Accelerate
           </h1>
-          <p className="text-2xl tracking-wide text-black font-medium mt-5">
+          <p className="text-2xl md:text-3xl tracking-wide text-black font-medium mt-5">
             We Revolutionize Customer Experience
           </p>
-          <p className="text-2xl tracking-wide text-black font-medium">
+          <p className="text-2xl md:text-3xl tracking-wide text-black font-medium">
             Using Technology, Data & Science.
           </p>
         </motion.div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,8 +11,8 @@ function Navbar() {
   };
 
   return (
-    <nav className="sticky z-50 w-full h-20  border-b-2 border-neutral-400 bg-white top-0 ">
-      <div className="flex justify-between items-center h-full w-full px-6 2xl:px-16">
+    <nav className="sticky z-50 w-full h-20 border-b-2 border-neutral-400 bg-white top-0">
+      <div className="flex justify-between items-center h-full w-full px-6 lg:px-16">
         <div>
           <div className="w-40 lg:w-60">
             <Link href="/">
@@ -21,49 +21,45 @@ function Navbar() {
           </div>
         </div>
         <div>
-          <ul className="hidden sm:flex items-center">
+          <ul className="hidden md:flex items-center">
             <Link href="/">
-              <li className="ml-14 hover:border-b text-xl hover:text-pink-500">
+              <li className="ml-8 lg:ml-14 hover:border-b text-lg lg:text-xl hover:text-pink-500">
                 Home
               </li>
             </Link>
-
             <Link href="/About">
-              <li className="ml-14 hover:border-b text-xl hover:text-pink-500">
+              <li className="ml-8 lg:ml-14 hover:border-b text-lg lg:text-xl hover:text-pink-500">
                 About
               </li>
             </Link>
-
             <Link href="/Services">
-              <li className="ml-14 hover:border-b text-xl hover:text-pink-500">
+              <li className="ml-8 lg:ml-14 hover:border-b text-lg lg:text-xl hover:text-pink-500">
                 Services
               </li>
             </Link>
-
             <Link href="/Clients">
-              <li className="ml-14 hover:border-b text-xl hover:text-pink-500">
+              <li className="ml-8 lg:ml-14 hover:border-b text-lg lg:text-xl hover:text-pink-500">
                 Clients
               </li>
             </Link>
-
             <Link href="https://share.hsforms.com/1zBadTPyLTVu585sGj-a5TQcyk5y">
-              <li className="ml-20 text-lg">
-                <button className="bg-black text-white px-8 py-4 rounded-lg hover:text-pink-500 hover:bg-white hover:shadow-sm hover:shadow-slate-600">
+              <li className="ml-12 lg:ml-20 text-lg">
+                <button className="bg-black text-white px-6 py-3 lg:px-8 lg:py-4 rounded-lg hover:text-pink-500 hover:bg-white hover:shadow-sm hover:shadow-slate-600">
                   Contact Us
                 </button>
               </li>
             </Link>
           </ul>
         </div>
-        <div onClick={handleNav} className="lg:hidden cursor-pointer pl-24">
+        <div onClick={handleNav} className="md:hidden cursor-pointer pl-24">
           <AiOutlineMenu size={25} />
         </div>
       </div>
       <div
         className={
           menuOpen
-            ? "fixed right-0 z-50 top-0 w-[100%] sm:hidden h-screen bg-[#f7f7f7] p-10 ease-in duration-500 flex justify-center items-center"
-            : "fixed right-[-100%] z-50 w-[100%] top-0 p-10   ease-in duration-200 flex justify-center items-center"
+            ? "fixed right-0 z-50 top-0 w-[100%] md:hidden h-screen bg-[#f7f7f7] p-10 ease-in duration-500 flex justify-center items-center"
+            : "fixed right-[-100%] z-50 w-[100%] top-0 p-10 ease-in duration-200 flex justify-center items-center"
         }
       >
         <div className="absolute top-10 right-10">
@@ -71,10 +67,9 @@ function Navbar() {
             <AiOutlineClose size={25} />
           </div>
         </div>
-
         <div className="flex flex-col py-4 text-center">
           <ul>
-            <li className=" mb-10 cursor-pointer">
+            <li className="mb-10 cursor-pointer">
               <div className="w-40">
                 <Link href="/">
                   <Image alt="LOGO" src="/TF.png" width={1351} height={184} />
