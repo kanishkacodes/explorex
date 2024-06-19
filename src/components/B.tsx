c5e9fb7efe8cc84db4c3d5fa3212b4fa4985bc75
Kanishka Kumar <kumarkanishka2111@gmail.com>
2024-06-19 13:12:57 +0530

    css fixed

--- src/components/B.tsx
+++ src/components/B.tsx
@@ -1,71 +1,48 @@
-"use client";
-import React, { useEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import React from 'react'
 import LottieAnimation from "@/components/LottieAnimation";
 
-const elementVariants = {
-  hidden: { opacity: 0, y: 75 },
-  visible: { opacity: 1, y: 0 },
-};
-
-const B: React.FC = () => {
-  const controls = useAnimation();
-  const [ref, inView] = useInView({ triggerOnce: true });
-
-  useEffect(() => {
-    if (inView) {
-      controls.start("visible");
-    }
-  }, [controls, inView]);
-
-  const textElements = [
-    "Promotions",
-    "Staff Training",
-    "Social Media Presence",
-    "Voice of Customer",
-    "Feedback Analysis"
-  ];
-
+function B() {
   return (
-    <div ref={ref} className="md:flex items-center">
-      <motion.div
-        className="w-full md:w-1/2 flex justify-start space-x-5 md:space-x-10 md:ml-80 "
-        initial="hidden"
-        animate={controls}
-        variants={elementVariants}
-        transition={{ duration: 0.5, delay: 0.1 }}
-      >
-        <div className="md:w-16 md:h-16 w-12 h-12 rounded-full bg-purple-500 ml-5 md:ml-0"></div>
-        <div>
-          <h1 className="font-bold text-5xl md:text-7xl text-black mb-8">Business</h1>
-          <div className="md:text-2xl text-xl tracking-wide text-black font-medium ml-5">
-            {textElements.map((text, index) => (
-              <motion.p
-                key={index}
-                initial="hidden"
-                animate={controls}
-                variants={elementVariants}
-                transition={{ duration: 0.5, delay:index * 0.1 }}
-                className="mb-5"
-              >
-                {text}
-              </motion.p>
-            ))}
-          </div>
-        </div>
-      </motion.div>
-      <motion.div
-        className="w-full md:w-1/2"
-        initial="hidden"
-        animate={controls}
-        variants={elementVariants}
-        transition={{ duration: 0.5, delay: 0.3 }}
-      >
-        <LottieAnimation animationPath="/Assets/business.json" />
-      </motion.div>
-    </div>
-  );
-};
+    <div>
+       
+       <div className=' flex justify-center'>
 
-export default B;
+
+        <div className='flex '>
+
+          <div className=' w-10 h-10 xl:w-14 xl:h-14 rounded-full bg-purple-500 mt-2 '></div>
+          
+          <div className=''>
+          <div className=' font-bold text-5xl xl:text-7xl text-black mb-5 ml-5'>
+              Business
+          </div>
+
+          <div className='xl:text-2xl text-xl tracking-wide text-black font-medium ml-9'>
+            <p className=' mb-5'>Promotions</p>
+            <p className=' mb-5'>Staff Training</p>
+            <p className=' mb-5'>Social Media Presence</p>
+            <p className=' mb-5'>Voice of Customer</p>
+            <p className=' mb-5'>Feedback Analysis</p>
+
+          </div>
+
+          </div>
+
+          <div>
+          <LottieAnimation animationPath="/Assets/business.json" />
+          </div>
+
+
+
+
+        </div>
+
+
+
+       </div>
+      
+    </div>
+  )
+}
+
+export default B
