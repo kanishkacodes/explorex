2bc65c4c7676c19897a282828e98962aae550268
Kanishka Kumar <kumarkanishka2111@gmail.com>
2024-07-15 16:29:57 +0530

    powered by

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Thinkfluence",
+  title: "Thinkfluence-powered by Explorex",
   description: "Thinkfluence",
 };
 
